d9d3a0e80171b9017d70575ed2c3fa5a9e143185
Kenson Man <kenson@kenson.idv.hk>
2016-04-09 22:00:33 +0800

    Complete the demo

--- demo.py
+++ demo.py
@@ -6,45 +6,72 @@
 #
 import socket
 import time
+import thread
 from picamera import PiCamera
 from RPi import GPIO
 
 #setup
+bind='0.0.0.0'
 width=640
 height=480
+framerate=24
 port=8000
 pin_yellow=23
 pin_red=24
+pin_btn=25
+global recording
+recording=False
 
 def init():
     print('Initializes the program...')
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin_yellow, GPIO.OUT)
     GPIO.setup(pin_red, GPIO.OUT)
+    GPIO.setup(pin_btn, GPIO.IN)
     
 def setready(status):
     print('Setting status: %s'%status)
     GPIO.output(pin_yellow, status)
     GPIO.output(pin_red, not status)
+
+def checkbtn():
+	'''
+	Check an change btn status
+	'''
+	global recording
+	while True:
+		if GPIO.input(pin_btn)==1:
+			print('Captured btn clicked')
+			recording=not recording
+			if recording:
+				thread.start_new_thread(stream, ())
+		time.sleep(0.5)
+
+def stream():
+	global recording
+	if not recording: return
+	setready(False)
+	# Streaming 
+	with PiCamera() as camera:
+		camera.resolution=(width,height)
+		camera.framerate=framerate
+
+		sock=socket.socket()
+		sock.bind((bind, port))
+		sock.listen(0)
+
+		conn=sock.accept()[0].makefile('wb')
+		try:
+			camera.start_recording(conn, format='h264')
+			while recording:
+				print('recording')
+				camera.wait_recording(1)
+			camera.stop_recording()
+		finally:
+			conn.close()
+			sock.close()
+			setready(True)
     
 init()
 setready(True)
-
-# Streaming 
-with PiCamera() as camera:
-  camera.resolution=(640,480)
-  camera.framerate=24
-  
-  sock=socket.socket()
-  sock.bind(('0.0.0.0',8000))
-  sock.listen(0)
-  
-  c=sock.accept()[0].makefile('wb')
-  try:
-    camera.start_recording(c, format='h264')
-    camera.wait_recording(60)
-    camera.stop_recording()
-  finally:
-    c.close()
-    sock.close()
-    setready(False)
+checkbtn()
